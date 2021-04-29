2fbfa25a9d12984130e8ff009405b24a0eb36a21
AlanDavd <alandavidrl11@gmail.com>
2021-04-29 17:53:27 -0500

    add UserCreationForm tests

--- morph/users/tests/test_forms.py
+++ morph/users/tests/test_forms.py
@@ -1,39 +1,37 @@
-"""
-Module for all Form Tests.
-"""
-import pytest
-from django.utils.translation import ugettext_lazy as _
+"""Module for all Form Tests."""
 
+# Forms
 from morph.users.forms import UserCreationForm
+
+# Models
 from morph.users.models import User
+
+# Pytest
+import pytest
 
 pytestmark = pytest.mark.django_db
 
 
 class TestUserCreationForm:
-    """
-    Test class for all tests related to the UserCreationForm
-    """
+    """Test class for all tests related to the UserCreationForm."""
 
     def test_username_validation_error_msg(self, user: User):
         """
         Tests UserCreation Form's unique validator functions correctly by testing:
             1) A new user with an existing username cannot be added.
-            2) Only 1 error is raised by the UserCreation Form
-            3) The desired error message is raised
+            2) A new user with an existing email cannot be added.
+            3) 1 or more errors are raised by the UserCreation Form
+            4) The desired error message is raised
         """
 
-        # The user already exists,
-        # hence cannot be created.
-        form = UserCreationForm(
-            {
-                "username": user.username,
-                "password1": user.password,
-                "password2": user.password,
-            }
-        )
+        # The user already exists, hence cannot be created.
+        form = UserCreationForm({
+            "username": user.username,
+            "password": user.password,
+            "password_confirmation": user.password,
+        })
 
         assert not form.is_valid()
-        assert len(form.errors) == 1
+        assert len(form.errors) == 4
         assert "username" in form.errors
-        assert form.errors["username"][0] == _("This username has already been taken.")
+        assert form.errors["username"][0] == "Username is already in use."
