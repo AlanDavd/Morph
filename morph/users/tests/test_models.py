3c2a019f5670f8e1bd643baf7488c14a9201a97c
AlanDavd <alandavidrl11@gmail.com>
2021-04-29 17:56:17 -0500

    add model test

--- morph/users/tests/test_models.py
+++ morph/users/tests/test_models.py
@@ -1,9 +1,13 @@
+"""User test related to model."""
+
+# Pytest
 import pytest
 
+# Models
 from morph.users.models import User
 
 pytestmark = pytest.mark.django_db
 
 
-def test_user_get_absolute_url(user: User):
-    assert user.get_absolute_url() == f"/users/{user.username}/"
+def test_user_get_short_name(user: User):
+    assert user.get_short_name() == user.username
