a08c044c9b992e05e1c08897aac25af256e211f9
AlanDavd <alandavidrl11@gmail.com>
2021-04-29 19:04:44 -0500

    fix tests

--- morph/users/tests/test_forms.py
+++ morph/users/tests/test_forms.py
@@ -19,9 +19,8 @@ class TestUserCreationForm:
         """
         Tests UserCreation Form's unique validator functions correctly by testing:
             1) A new user with an existing username cannot be added.
-            2) A new user with an existing email cannot be added.
-            3) 1 or more errors are raised by the UserCreation Form
-            4) The desired error message is raised
+            2) 1 or more errors are raised by the UserCreation Form
+            3) The desired error message is raised
         """
 
         # The user already exists, hence cannot be created.
@@ -35,3 +34,23 @@ class TestUserCreationForm:
         assert len(form.errors) == 4
         assert "username" in form.errors
         assert form.errors["username"][0] == "Username is already in use."
+
+    def test_email_validation_error_msg(self, user: User):
+        """
+        Tests UserCreation Form's unique validator functions correctly by testing:
+            1) A new user with an existing email cannot be added.
+            2) 1 or more errors are raised by the UserCreation Form
+            3) The desired error message is raised
+        """
+
+        # The user already exists, hence cannot be created.
+        form = UserCreationForm({
+            "username": user.username,
+            "password": user.password,
+            "password_confirmation": user.password,
+        })
+
+        assert not form.is_valid()
+        assert len(form.errors) == 4
+        assert "email" in form.errors
+        assert form.errors["email"][0] == "This field is required."
